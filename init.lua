bf64ede7da8ef9cce3ea1341c3a58694724ce157
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-11 18:32:58 -0300

    fix: commands

--- init.lua
+++ init.lua
@@ -1,17 +1,20 @@
+local autocmd = vim.api.nvim_create_autocmd
+local command = vim.api.nvim_create_user_command
 
-    vim.api.nvim_create_autocmd("User AstroFile", {
-      desc = "no auto comment after pressing o",
-      pattern = "*",
-      command = "setlocal formatoptions-=o",
-    })
+-- Disable continuation comment on next line
+autocmd("User", {
+  desc = "no auto comment after pressing o",
+  pattern = "*",
+  command = "setlocal formatoptions-=cro",
+})
 
-    -- Open Github repository
-    vim.api.nvim_create_user_command("OpenGithubRepo", function(_)
-      local ghpath = vim.api.nvim_eval "shellescape(expand('<cfile>'))"
-      local formatpath = ghpath:sub(2, #ghpath - 1)
-      local repourl = "https://www.github.com/" .. formatpath
-      vim.fn.system { "xdg-open", repourl }
-    end, {
-      desc = "Open Github Repo",
-      force = true,
-    })
+-- Open Github repository
+command("OpenGithubRepo", function(_)
+  local ghpath = vim.api.nvim_eval "shellescape(expand('<cfile>'))"
+  local formatpath = ghpath:sub(2, #ghpath - 1)
+  local repourl = "https://www.github.com/" .. formatpath
+  vim.fn.system { "xdg-open", repourl }
+end, {
+  desc = "Open Github Repo",
+  force = true,
+})
