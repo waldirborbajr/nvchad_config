7914615ccc0b606bd652673bfe8f638f136640fa
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-11 18:58:36 -0300

    feat: LazyGit plugin

--- plugins.lua
+++ plugins.lua
@@ -117,6 +117,16 @@ local plugins = {
   {
     "echasnovski/mini.move"
   },
+  {
+    "kdheepak/lazygit.nvim",
+    keys={
+      {"<leader>gg","<cmd>LazyGit<cr>", desc="LazyGit"},
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
 
 
