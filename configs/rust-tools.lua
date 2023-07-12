0e8e697bd151151f609d5caef4fe32e952754017
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-12 16:10:20 -0300

    feat: rust-tools setup

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -1,12 +1,18 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+local rt = require("rust-tools")
 
 local options = {
   server = {
-    on_attach = on_attach,
     capabilities = capabilities,
-  }
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
 }
 
 return options
