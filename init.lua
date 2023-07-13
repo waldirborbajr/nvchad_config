256152ff2719cf64f0f9edd49671d7a775987fc7
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-13 13:28:19 -0300

    feat: vim cmd

--- init.lua
+++ init.lua
@@ -21,3 +21,9 @@ end, {
   force = true,
 })
 
+vim.cmd [[
+    augroup format_on_save
+      autocmd! 
+      autocmd BufWritePre * lua vim.lsp.buf.format({ async = false }) 
+    augroup end
+  ]]
