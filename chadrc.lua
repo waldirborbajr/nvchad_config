1378f97992746e0044ff2fc0de216715d828f17c
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-13 08:26:50 -0300

    feat: theme

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,17 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'nord'}
- M.plugins = "custom.plugins"
- M.mappings = require("custom.mappings")
- return M
+local M = {}
+
+M.ui = {
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+
+  transparency = false,
+}
+
+M.plugins = "custom.plugins"
+
+M.mappings = require "custom.mappings"
+
+M.format_on_save = true
+
+return M
