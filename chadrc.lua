6622bedfdf7f96000e5d929b2c04f64436d88224
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-17 20:10:07 -0300

    feat: change theme

--- chadrc.lua
+++ chadrc.lua
@@ -2,16 +2,13 @@
 local M = {}
 
 M.ui = {
-  theme_toggle = { "catppuccin", "catppuccin_latte" },
-  theme = "catppuccin",
-
+  theme_toggle = { "onenord", "onenord_latte" },
+  theme = "onenord",
   transparency = false,
 }
 
 M.plugins = "custom.plugins"
 
 M.mappings = require "custom.mappings"
-
-M.format_on_save = 1 
 
 return M
