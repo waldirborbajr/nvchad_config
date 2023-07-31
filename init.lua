2e94d5f379e01e960b07f2649e91331f9ce3e139
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-07-31 13:01:56 -0300

    feat(format): go

--- init.lua
+++ init.lua
@@ -44,6 +44,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("go.format").goimport()
   end,
 })
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     vim.lsp.buf.code_action { context = { only = { "source.organizeImports" } }, apply = true }
+--   end,
+-- })
 
 vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
 vim.api.nvim_create_autocmd("LspAttach", {
