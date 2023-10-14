262eddef1d8c176d80f2b1b495ce9d42477e33a7
Waldir Borba Junior <wborbajr@gmail.com>
2023-10-14 09:05:05 -0300

    fix: gopher.nvim setup

--- plugins.lua
+++ plugins.lua
@@ -64,10 +64,17 @@ local plugins = {
   -- GO
   {
     "olexsmir/gopher.nvim",
+    ft = "go",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
   },
   {
     "ray-x/go.nvim",
