5c788617f4cb63369130cc87c110b2b4f64b75d4
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-08 15:53:40 -0300

    1699469620/screeching-shelf

--- plugins.lua
+++ plugins.lua
@@ -266,16 +266,77 @@ local plugins = {
   -- Rust
   {
     "simrat39/rust-tools.nvim",
-    after = "nvim-lspconfig",
     ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function()
-      return require "custom.configs.rust-tools"
-    end,
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
+    config = function()
+      require("rust-tools").setup {
+        tools = {
+          runnables = {
+            use_telescope = true,
+          },
+          inlay_hints = {
+            auto = true,
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = "",
+          },
+        },
+        --
+        -- all the opts to send to nvim-lspconfig
+        -- these override the defaults set by rust-tools.nvim
+        -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+        server = {
+          on_attach = function(_, buffer)
+            require("core.utils").load_mappings "lspconfig"
+          end,
+
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+              },
+              assist = {
+                importEnforceGranularity = true,
+                importPrefix = "crate",
+              },
+              checkOnSave = {
+                command = "clippy",
+              },
+              -- inlayHints = { locationLinks = false },
+              diagnostics = {
+                enable = true,
+                experimental = {
+                  enable = true,
+                },
+              },
+            },
+          },
+
+          -- settings = {
+          --   -- to enable rust-analyzer settings visit:
+          --   -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+          --   ["rust-analyzer"] = {
+          --     -- enable clippy on save
+          --     checkOnSave = {
+          --       command = "clippy",
+          --     },
+          --   },
+          -- },
+        },
+      }
     end,
   },
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  --   after = "nvim-lspconfig",
+  --   ft = "rust",
+  --   dependencies = "neovim/nvim-lspconfig",
+  --   opts = function()
+  --     return require "custom.configs.rust-tools"
+  --   end,
+  --   config = function(_, opts)
+  --     require("rust-tools").setup(opts)
+  --   end,
+  -- },
   {
     "saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
