c27f7132d3c292b338fb4e5307a573d056daa89c
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-01-30 17:51:42 -0300

    feat: new commands

--- autocmds.lua
+++ autocmds.lua
@@ -1,16 +1,22 @@
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.api.nvim_command
+local augroup = vim.api.nvim_create_augroup
+-- local settings = require("custom.chadrc").settings
+local fn = vim.fn
+
 local function augroup(name)
   return vim.api.nvim_create_augroup("lit_" .. name, { clear = true })
 end
 
 -- Disable continuation comment on next line
-vim.api.nvim_create_autocmd("User", {
+autocmd("User", {
   desc = "no auto comment after pressing o",
   pattern = "*",
   command = "setlocal formatoptions-=cro",
 })
 
 -- Remember cursor position when reopening files go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
+autocmd("BufReadPost", {
   group = augroup "last_loc",
   callback = function(event)
     local exclude = { "gitcommit" }
@@ -52,7 +58,7 @@ vim.api.nvim_create_user_command("Format", function(args)
 end, { range = true })
 
 -- highlight on yank
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local highlight_group = augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
@@ -64,7 +70,69 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Autoreload on buffer change
 -- https://stackoverflow.com/questions/62100785/auto-reload-file-and-in-neovim-and-auto-reload-nerbtree
 vim.o.autoread = true
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
 })
+
+autocmd({ "BufRead" }, {
+  desc = "Display a message when the current file is not in utf-8 format",
+  pattern = "*",
+  group = augroup("non_utf8_file", { clear = true }),
+  callback = function()
+    if vim.bo.fileencoding ~= "utf-8" then
+      vim.notify("File not in UTF-8 format!", vim.log.levels.WARN, { title = "nvim-config" })
+    end
+  end,
+})
+
+autocmd("FileType", {
+  desc = "Close NvimTree before quit nvim",
+  pattern = { "NvimTree" },
+  callback = function(args)
+    autocmd("VimLeavePre", {
+      callback = function()
+        vim.api.nvim_buf_delete(args.buf, { force = true })
+        return true
+      end,
+    })
+  end,
+})
+
+autocmd("BufWritePost", {
+  desc = "Reload NvimTree after writing the buffer",
+  callback = function()
+    local bufs = fn.getbufinfo()
+    for _, buf in ipairs(bufs) do
+      if buf.name:find "NvimTree_" then
+        cmd "NvimTreeRefresh"
+        break
+      end
+    end
+  end,
+})
+
+-- Nvimtree open file on creation
+local function open_file_created()
+  require("nvim-tree.api").events.subscribe("FileCreated", function(file)
+    vim.cmd("edit " .. file.fname)
+  end)
+end
+
+autocmd({ "VimEnter" }, {
+  callback = open_file_created,
+})
+
+-- prevent comment from being inserted when entering new line in existing comment
+autocmd("BufEnter", {
+  callback = function()
+    -- allow <CR> to continue block comments only
+    -- https://stackoverflow.com/questions/10726373/auto-comment-new-line-in-vim-only-for-block-comments
+    vim.opt.comments:remove "://"
+    vim.opt.comments:remove ":--"
+    vim.opt.comments:remove ":#"
+    vim.opt.comments:remove ":%"
+  end,
+})
+
+-- https://github.com/BrunoKrugel/dotfiles/blob/master/utils/autocmd.lua
