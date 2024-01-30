4260c9666b7d4e061842886fa063a3fa3d7c46d3
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-01-30 17:40:12 -0300

    feat: testing noice new setup

--- configs/noice.lua
+++ configs/noice.lua
@@ -1,34 +1,106 @@
 return {
   cmdline = {
-    enabled = true, -- enables the Noice cmdline UI
-    view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-    opts = {}, -- global options for the cmdline. See section on views
+    enabled = true,
+    view = "cmdline_popup",
     format = {
-      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-      -- view: (default is cmdline view)
-      -- opts: any options passed to the view
-      -- icon_hl_group: optional hl_group for the icon
-      -- title: set to anything or empty string to hide
-      cmdline = { pattern = "^:", icon = "", lang = "vim" },
-      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      cmdline = { pattern = "^:", icon = "󰘳 ", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = "󰩊 ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = "󰩊 ", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "󰻿 ", lang = "bash" },
       lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-      help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
-      input = {}, -- Used by input()
-      -- lua = false, -- to disable a format, set to `false`
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "󰞋 " },
     },
+  },
+  popupmenu = {
+    enabled = true, -- enables the Noice popupmenu UI
+    backend = "nui", -- backend to use to show regular cmdline completions
   },
   routes = {
     {
-      view = "notify",
-      filter = { event = "msg_showmode" },
+      filter = {
+        event = "lsp",
+        any = {
+          { find = "formatting" },
+          { find = "Diagnosing" },
+          { find = "Diagnostics" },
+          { find = "diagnostics" },
+          { find = "code_action" },
+          { find = "Processing full semantic tokens" },
+          { find = "symbols" },
+          { find = "completion" },
+        },
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "notify",
+        any = {
+          -- Neo-tree
+          { find = "Toggling hidden files: true" },
+          { find = "Toggling hidden files: false" },
+          { find = "Operation canceled" },
+          { find = "^No code actions available$" },
+
+          -- Telescope
+          { find = "Nothing currently selected" },
+          { find = "^No information available$" },
+          { find = "Highlight group" },
+          { find = "no manual entry for" },
+          { find = "not have parser for" },
+
+          -- ts
+          { find = "_ts_parse_query" },
+        },
+      },
+      opts = { skip = true },
     },
     {
       filter = {
         event = "msg_show",
         kind = "",
-        find = "written",
+        any = {
+
+          -- Edit
+          { find = "%d+ less lines" },
+          { find = "%d+ fewer lines" },
+          { find = "%d+ more lines" },
+          { find = "%d+ change;" },
+          { find = "%d+ line less;" },
+          { find = "%d+ more lines?;" },
+          { find = "%d+ fewer lines;?" },
+          { find = '".+" %d+L, %d+B' },
+          { find = "%d+ lines yanked" },
+          { find = "^Hunk %d+ of %d+$" },
+          { find = "%d+L, %d+B$" },
+
+          -- Save
+          { find = " bytes written" },
+
+          -- Redo/Undo
+          { find = " changes; before #" },
+          { find = " changes; after #" },
+          { find = "1 change; before #" },
+          { find = "1 change; after #" },
+
+          -- Yank
+          { find = " lines yanked" },
+
+          -- Move lines
+          { find = " lines moved" },
+          { find = " lines indented" },
+
+          -- Bulk edit
+          { find = " fewer lines" },
+          { find = " more lines" },
+          { find = "1 more line" },
+          { find = "1 line less" },
+
+          -- General messages
+          { find = "Already at newest change" },
+          { find = "Already at oldest change" },
+          { find = "E21: Cannot make changes, 'modifiable' is off" },
+        },
       },
       opts = { skip = true },
     },
@@ -36,12 +108,37 @@ return {
   lsp = {
     progress = {
       enabled = false,
-      -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
-      -- See the section on formatting for more details on how to customize.
-      format = "lsp_progress",
-      format_done = "lsp_progress_done",
-      throttle = 1000 / 30, -- frequency to update lsp progress message
-      view = "mini",
+    },
+    signature = {
+      enabled = true,
+      auto_open = {
+        enabled = true,
+        trigger = true,
+        luasnip = true,
+        throttle = 50,
+      },
+      view = nil, -- when nil, use defaults from documentation
+      ---@type NoiceViewOptions
+      opts = {
+        focusable = false,
+        size = {
+          max_height = 15,
+          max_width = 60,
+        },
+        win_options = {
+          wrap = false,
+        },
+      },
+    },
+    hover = {
+      silent = true,
+    },
+    documentation = {
+      opts = {
+        border = {
+          padding = { 0, 0 },
+        },
+      },
     },
     override = {
       -- override the default lsp markdown formatter with Noice
@@ -51,48 +148,46 @@ return {
       -- override cmp documentation with Noice (needs the other options to work)
       ["cmp.entry.get_documentation"] = true,
     },
-    hover = {
-      enabled = false,
-      silent = false, -- set to true to not show a message if hover is not available
-      view = nil, -- when nil, use defaults from documentation
-      opts = {}, -- merged with defaults from documentation
-    },
-    signature = {
-      enabled = false,
-      auto_open = {
-        enabled = true,
-        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-        throttle = 50, -- Debounce lsp signature help request by 50ms
+  },
+  ---@type NoiceConfigViews
+  views = {
+    cmdline_popup = {
+      position = {
+        row = 3,
+        col = "50%",
       },
-      view = nil, -- when nil, use defaults from documentation
-      opts = {}, -- merged with defaults from documentation
-    },
-    message = {
-      -- Messages shown by lsp servers
-      enabled = true,
-      view = "notify",
-      opts = {},
-    },
-    -- defaults for hover and signature help
-    documentation = {
-      view = "hover",
-      opts = {
-        lang = "markdown",
-        replace = true,
-        render = "plain",
-        format = { "{message}" },
-        win_options = { concealcursor = "n", conceallevel = 3 },
+      size = {
+        width = 60,
+        height = "auto",
       },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = 8,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
+    },
+    mini = {
+      zindex = 100,
+      win_options = { winblend = 0 },
     },
   },
   presets = {
-    -- you can enable a preset by setting it to true, or a table that will override the preset config
-    -- you can also add custom presets that you can enable/disable with enabled=true
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = true, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
+    bottom_search = true,
+    command_palette = true,
+    lsp_doc_border = true,
+    inc_rename = true,
   },
 }
