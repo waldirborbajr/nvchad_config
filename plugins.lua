a2663572cf11ded9b45eae2f00c51888d7c4902b
Waldir Borba Junior <wborbajr@gmail.com>
2024-02-16 08:23:04 -0300

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -119,14 +119,13 @@ local plugins = {
       require "custom.configs.rustaceanvim"
     end
   },
-  -- deprecated
-  -- {
-  --   "rust-lang/rust.vim",
-  --   ft = "rust",
-  --   init = function()
-  --     vim.g.rustfmt_autosave = 1
-  --   end,
-  -- },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
   {
     "simrat39/rust-tools.nvim",
     ft = "rust",
