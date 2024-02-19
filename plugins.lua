587c86e563c4994152b4a5c9d9b6d820ed5753f0
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-18 21:20:37 -0300

    Rand Message: /macabre-paint

--- plugins.lua
+++ plugins.lua
@@ -196,22 +196,73 @@ local plugins = {
   -- Obsidian
   {
     "epwalsh/obsidian.nvim",
-    version = "*",
-    event = "VeryLazy",
-    ft = "markdown",
-    dependencies = { -- optional packages
-      "nvim-lua/plenary.nvim",
+    -- the obsidian vault in this default config  ~/obsidian-vault
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+    -- event = { "bufreadpre " .. vim.fn.expand "~" .. "/my-vault/**.md" },
+    event = { "BufReadPre  */2ndBrain/*.md" },
+    keys = {
+      {
+        "gf",
+        function()
+          if require("obsidian").util.cursor_on_markdown_link() then
+            return "<cmd>ObsidianFollowLink<CR>"
+          else
+            return "gf"
+          end
+        end,
+        noremap = false,
+        expr = true,
+      },
     },
-    config = function()
-      require("obsidian").setup {
-        dir = "~/wks/2ndBrain/",
-        disable_frontmatter = true,
-        completion = {
-          nvim_cmp = true,
-        },
-      }
-    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      dir = "~/home/borba/wks/2ndBrain", -- specify the vault location. no need to call 'vim.fn.expand' here
+      use_advanced_uri = true,
+      finder = "telescope.nvim",
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+      },
+      note_frontmatter_func = function(note)
+        -- This is equivalent to the default frontmatter function.
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      follow_url_func = vim.ui.open,
+    },
   },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   event = "VeryLazy",
+  --   ft = "markdown",
+  --   dependencies = { -- optional packages
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   config = function()
+  --     require("obsidian").setup {
+  --       dir = "~/wks/2ndBrain/",
+  --       disable_frontmatter = true,
+  --       completion = {
+  --         nvim_cmp = true,
+  --       },
+  --     }
+  --   end,
+  -- },
 
   {
     "hrsh7th/nvim-cmp",
